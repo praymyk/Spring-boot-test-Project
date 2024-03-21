14ee0d6970e874e981a276aceadadbfb48a2eb47
Myks <praymyk@gmail.com>
2024-03-21 17:48:34 +0900

    first commit

---
+++ src/test/java/com/ys/firstproject/FirstprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ys.firstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
